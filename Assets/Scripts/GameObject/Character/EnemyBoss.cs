4326cd99e1d68ee8401500743c1e6a7abb68bf75
XaviXCC <xxwcn2014@gmail.com>
2026-03-17 14:20:55 -0400

    Enemy Boss & Minion

--- Assets/Scripts/GameObject/Character/EnemyBoss.cs
+++ Assets/Scripts/GameObject/Character/EnemyBoss.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class EnemyBoss : MonoBehaviour
+public class EnemyBoss : EnemyBase
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public new float health = 100f;                                                  // Boss health points
 }
